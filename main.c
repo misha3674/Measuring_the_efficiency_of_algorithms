f66ccd1da7833e4d66977d1afdeae72dd1410cd7
Misha Maydanskiy <maydanskiy.misha@gmail.com>
2017-03-29 12:23:08 +0300

    add bacbone program

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+typedef enum
+{
+    SETTINGS = 0,
+    RESULTS
+}eState;
+
+eState state = SETTINGS;
 
 int main()
 {
+    innit_openGL(); // return window
+    innit_settings(); // all that need calculate before loop
+    init_results();   // // all that need calculate before loop
 
+    while( isRun() ) // check run window
+    {
+       state = getState();
+       switch(state)
+       {
+           case SETTINGS:
+               processing_settings();
+           break;
+           case RESULTS:
+               processing_results();
+           break;
+           default: break;
+       }
+    }
     return 0;
 }
