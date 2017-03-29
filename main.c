8b993fcee500ed877abd27234c75dfaf19271d44
Misha Maydanskiy <maydanskiy.misha@gmail.com>
2017-03-29 22:13:55 +0300

    modify backbone program

--- main.c
+++ main.c
@@ -1,31 +1,30 @@
-#include <stdio.h>
-typedef enum
-{
-    SETTINGS = 0,
-    RESULTS
-}eState;
-
-eState state = SETTINGS;
-
+#include "draw.h"
+#include "innit.h"
+eState state = WAITING;
+// callback
+//switch(state)
+//  check_buttonIsPress_change_state(*state)
 int main()
 {
-    innit_openGL(); // return window
-    innit_settings(); // all that need calculate before loop
-    init_results();   // // all that need calculate before loop
-
+    innit_openGL(&state); // save pointer in draw_file//for changed state in calback function
     while( isRun() ) // check run window
     {
-       state = getState();
-       switch(state)
+       switch(state) // state processing in the callback function
        {
            case SETTINGS:
-               processing_settings();
+               draw_settings_screen(); // return    waitnig OR settings
            break;
            case RESULTS:
-               processing_results();
+               draw_results_screen(); // return settings OR results
+           break;
+           case WAITING:
+               innit_settings(); //calculate before loop
+               init_results();   //calculate
+               draw_waiting_screen();//state = RESULTS;  // go to results
            break;
            default: break;
        }
     }
     return 0;
 }
+
